906818b25fb3ca323be238d1943bd64b9ff83b1c
user <user@DESKTOP-COJ9858>
2021-11-15 17:19:08 +0900

    예약변경디비

---
+++ hotel_prj/src/main/java/user_reservation/ReservationUpdate.java
@@ -0,0 +1,30 @@
+package user_reservation;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import team3_dao.GetJdbcTemplate;
+
+/**
+ * 예약취소시  예약 상태 Y  > N으로 변경
+ * @author user
+ */
+public class ReservationUpdate {
+
+	public int reservationFlag(int res_no) {
+		int cnt =0;
+		
+		GetJdbcTemplate gjt = GetJdbcTemplate.getInstance();
+		JdbcTemplate jt = gjt.getJdbcTemplate();
+		
+		String deleteRes =  "update reservation set res_status='N' where res_no=?";
+		cnt=jt.update(deleteRes,res_no);
+		gjt.closeAc();
+		
+		return cnt;
+	}//reservationFlag
+	
+	
+	
+	
+	
+}//ReservationUpdate
